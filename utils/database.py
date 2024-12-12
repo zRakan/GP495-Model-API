7ea744f617471833fbd72234bb46375dfbb1e38f
Rayan <124823271+itRayan@users.noreply.github.com>
2024-12-12 21:16:53 +0300

    Task #2
    Update database.py:
    Create a function to validate generated SQL using `sqlparse` package.
    
    function name: validate_sql()

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 import re
+import sqlparse
 
 connection = None
 
@@ -34,3 +35,22 @@ def extractSchema():
             return schema
         except pymysql.Error as err:
             raise Exception(err)
+        
+def validate_sql(query):
+    """
+    Validate the structure of an SQL query using sqlparse.
+    """
+    try:
+        # Parse the query, which returns a list of parsed statements.
+        parsed = sqlparse.parse(query)
+        if not parsed:
+            return False, "Invalid SQL: Could not parse query."
+        # Check that the query starts with a valid SQL command ( SELECT Only )
+        valid_starts = {'SELECT'}
+        first_token = parsed[0].tokens[0].value.upper() # parsed take first element in list, tokens is components of SQL statement ex: Select, *, From
+        if first_token not in valid_starts:
+            return False, f"Invalid SQL: Query must start with {', '.join(valid_starts)}."
+        return True, "Valid SQL"
+    
+    except Exception as e:
+        raise ValueError(f"SQL Validation Error: {str(e)}")
