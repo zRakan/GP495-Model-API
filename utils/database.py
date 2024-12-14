aadf77c07359fdb1075cad0e07903e0dfde358a4
Saad AlMasaad <127618889+SaadDevl@users.noreply.github.com>
2024-12-14 23:07:26 +0300

    fix: Remove display rows because it increases hallucination

--- utils/database.py
+++ utils/database.py
@@ -33,28 +33,8 @@ def extractSchema():
                 schema = cursor.fetchone()['Create Table']
                 cleaned_schema = re.sub(r" ENGINE.*", "", schema)
 
-                cursor.execute('SELECT * FROM ' + table_name + ' LIMIT 3')  
-                rows = cursor.fetchall()
-
                 # Add the schema to the result text
                 result_text += f"{cleaned_schema}\n\n"
-
-                # Add the rows as a comment
-                result_text += "/*\n"
-                result_text += f"{len(rows)} rows from {table_name} table:\n"
-
-                # Extract column names from the first row if rows exist
-                if rows:
-                    column_names = rows[0].keys()
-                    result_text += "\t".join(column_names) + "\n"
-
-                # Add the rows
-                for row in rows:
-                    row_values = [str(value) for value in row.values()]
-                    result_text += "\t".join(row_values) + "\n"
-
-                result_text += "*/\n\n"
-
             return result_text
         except pymysql.Error as err:
             raise Exception(err)
