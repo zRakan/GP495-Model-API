d1b1d3b8293c3510971bee9460559855705fd295
Rayan <124823271+itRayan@users.noreply.github.com>
2024-12-15 02:29:05 +0300

    feat: Add /generate-questions endpoint

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from utils import database
 
 app = FastAPI()
 
 @app.get('/')
-async def readSchema():
-    return database.extractSchema()
+@app.get('/generate-questions')
+async def generate_questions():
+    try:
+        schema = database.extract_schema()
+        questions = database.generate_questions(schema)
+        return {"questions": questions}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
