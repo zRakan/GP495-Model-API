838b1ee5ada39a7ed36281b46a124ebb688b00f9
zRakan <placeholder@this.gg>
2024-12-28 00:50:40 +0300

    ref: Singleton class for `GroqClient`

--- classes/Groq.py
+++ classes/Groq.py
@@ -4,9 +4,15 @@ from groq import Groq
 from utils.prompts import REWRITE_QUESTION
 
 class GroqClient:
-    def __init__(self, model_name):
-        self.client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-        self.model_name = model_name
+    _instance = None
+
+    def __new__(cls, model_name):
+        if not cls._instance:
+            cls._instance = super(GroqClient, cls).__new__(cls)
+            cls._instance.client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+            cls._instance.model_name = model_name
+
+        return cls._instance
 
     def generate(self, messages):
         """
