78707447f0e20b4346fb01aca186f91bbd3f175a
zRakan <placeholder@this.gg>
2024-12-28 22:12:14 +0300

    temp: API endpoints for testing the model

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 from fastapi import FastAPI, HTTPException
+
 from utils import database
+from utils import Agent
 
 app = FastAPI()
 
-@app.get('/')
-@app.get('/generate-questions')
+
+from pydantic import BaseModel
+class messageSend(BaseModel):
+    history: list
+    message: str
+
+@app.post('/sendMessage')
+async def sendMessage(message: messageSend):
+    try:
+        return Agent.sendPrompt(conversation=message.history, prompt=message.message)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+class messageRewrite(BaseModel):
+    previous: str
+    current: str
+
+@app.post('/rewriteMessage')
+async def rewriteMessage(message: messageRewrite):
+    try:
+        return Agent.rewriteQuestion(message.previous, message.current)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+class sqlRewrite(BaseModel):
+    input: str
+    invalidSQL: str
+
+@app.post('/fixSQL')
+async def fixSQL(message: sqlRewrite):
+    try:
+        schema = database.extractSchema()
+        return Agent.sqlSafeExecute(message.input, message.invalidSQL, schema)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get('/suggestions')
 async def generate_questions():
     try:
-        schema = database.extract_schema()
-        questions = database.generate_questions(schema)
+        schema = database.extractSchema()
+        questions = Agent.generateQuestions(schema)
         return {"questions": questions}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
