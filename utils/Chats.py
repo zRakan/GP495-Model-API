61a088bc549dfc6864817dadcb4a17ac3b298fb8
zRakan <placeholder@this.gg>
2024-12-28 22:08:55 +0300

    ref(Chats): Remove chat creation functionality

--- utils/Chats.py
+++ utils/Chats.py
@@ -10,30 +10,10 @@ Collection = Database['Chats']
 Collection.create_index('author')
 Collection.create_index('id')
 
-def createChannel(authorId, title):
-    try:
-        channelId = uuid4()
-        
-        Collection.insert_one({
-            "id": channelId,
-            "author": authorId,
-            "title": title,
-            "history": []
-        })
-
-        return channelId
-    except Exception as e:
-        raise Exception(e)
-
-
-def getMessages(chatId, withDiagrams=False):
+def getMessages(chatId):
     chat = Collection.find_one({ 'chatId': chatId })
     if(chat is None):
         raise None
-    
-    # Filter out diagrams
-    if(not withDiagrams):
-        chat['history'] = [ message for message in chat['history'] if "type" not in message ]
     
     return chat['history']
 
