bb5aa4c08876d479f21309442521867a5239fec1
zRakan <placeholder@this.gg>
2025-01-10 14:20:58 +0300

    feat(`qdrant`): Create a collection if doesn't exist

--- utils/RAG.py
+++ utils/RAG.py
@@ -1,8 +1,16 @@
 from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
 
+COLLECTION = "SQL"
 CLIENT = QdrantClient(host='localhost', port=6333) 
 
-def getRAG(input, collection="SQL", limit=10):
+if(not CLIENT.collection_exists(collection_name=COLLECTION)):
+    CLIENT.create_collection(
+        collection_name=COLLECTION,
+        vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+    )
+
+def getRAG(input, collection=COLLECTION, limit=10):
     search_result = CLIENT.query(
         collection_name=collection,
         query_text=input,
